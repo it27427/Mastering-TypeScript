529fc90888c408f3b3527c72f1b58592c9ee5c06
it27427 <istiak.tushar.qtec@gmail.com>
2022-06-21 02:40:40 +0600

    core types

---
+++ core/core.ts
@@ -0,0 +1,10 @@
+function addTwoNumber(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 3.14159;
+
+const sum = addTwoNumber(number1, number2);
+
+console.log(sum);

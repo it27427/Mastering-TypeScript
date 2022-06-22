4150d3b21766dec1cc6328cba30f98a331154bea
it27427 <istiak.tushar.qtec@gmail.com>
2022-06-22 23:16:20 +0600

    learning core type interface

--- core/core.js
+++ core/core.js
@@ -1,7 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function addTwoNumber(n1, n2, bool1, phrase) {
+    var result = n1 + n2;
+    if (bool1) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
-var num1 = 5;
-var num2 = 3;
-var result = add(num1, num2);
-console.log(result);
+var number1 = 5;
+var number2 = 3.14159;
+var bool1 = true;
+var phrase = 'The result is: ';
+addTwoNumber(number1, number2, bool1, phrase);
+// console.log(sum);
--- core/core.ts
+++ core/core.ts
@@ -1,10 +1,19 @@
-function addTwoNumber(n1: number, n2: number) {
-  return n1 + n2;
+function addTwoNumber(n1: number, n2: number, bool1: boolean, phrase: string) {
+  const result = n1 + n2;
+  
+  if(bool1) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
 const number2 = 3.14159;
+const bool1 = true;
+const phrase = 'The result is: ';
 
-const sum = addTwoNumber(number1, number2);
 
-console.log(sum);
+addTwoNumber(number1, number2, bool1, phrase);
+
+// console.log(sum);
